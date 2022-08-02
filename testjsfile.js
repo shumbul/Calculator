e0fc8fd0e79e034e0d53307ab0ccc88f5ade845b
Shumbul Arifa <arifas18922@gmail.com>
2022-08-03 00:03:54 +0530

    beautify code

--- testjsfile.js
+++ testjsfile.js
@@ -1,17 +1,15 @@
-function roi()
-{
-var b=document.getElementById("In_b").value;
-var s =b * 3 * 22;
- 
- var e = document.getElementById("In_e").value;
-     t =e*1.5*22
+function roi() {
+    var b = document.getElementById("In_b").value;
+    var s = b * 3 * 22;
 
-var c=document.getElementById("In_c").value;
-var d=document.getElementById("In_d").value;
-var t =h*33
-var m=i/60;
-var o= [(s*m)-(t*m)]*g
+    var e = document.getElementById("In_e").value;
+    t = e * 1.5 * 22
 
-document.getElementById("out_o").innerHTML=o;
+    var c = document.getElementById("In_c").value;
+    var d = document.getElementById("In_d").value;
+    var t = h * 33
+    var m = i / 60;
+    var o = [(s * m) - (t * m)] * g
+
+    document.getElementById("out_o").innerHTML = o;
 }
-
