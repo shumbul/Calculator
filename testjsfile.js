175b45f51bf2a666437e4b78e9f1c233e0d9faf9
Zarkashankhan <110449250+Zarkashankhan@users.noreply.github.com>
2022-08-02 17:32:40 +0530

    Add files via upload

---
+++ testjsfile.js
@@ -0,0 +1,17 @@
+function roi()
+{
+var b=document.getElementById("In_b").value;
+var s =b * 3 * 22;
+ 
+ var e = document.getElementById("In_e").value;
+     t =e*1.5*22
+
+var c=document.getElementById("In_c").value;
+var d=document.getElementById("In_d").value;
+var t =h*33
+var m=i/60;
+var o= [(s*m)-(t*m)]*g
+
+document.getElementById("out_o").innerHTML=o;
+}
+
